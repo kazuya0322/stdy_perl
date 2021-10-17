75119235ec55f6c83175483e1c302d3b34d7ce22
takayama0322 <kazuya.takayama18@gmail.com>
2021-10-17 18:17:32 +0900

    page39

--- hello.pl
+++ hello.pl
@@ -14,3 +14,19 @@ print "\n";
 
 print "二重引用符で変数　$discount_price 表示\n";
 print "\$discount_price = $discount_price \n";
+
+# 10→16新数
+my $value = 255;
+my $str = sprintf('%x', $value);
+print $str . "\n";
+
+foreach my $value (0..255) {
+    my $str = sprintf("%x" , $value);
+    print $value , ' -> ' , $str , "\n";
+}
+
+# 16文字列を→10進数へ。
+print "FF -> " , hex("ff") , "\n";
+print "10 -> " , hex("10") , "\n";
+print "10 -> " , hex(10) , "\n";
+
--- string.pl
+++ string.pl
@@ -13,3 +13,19 @@ print "連結した文字列：$str1" . " " . $str2 . "\n";
 # You must write \" when you want a " in a string.【改行】
 print "You must write \\\" when you want a \" in a string.\n";
 
+# substr 置換
+my $str = "Programming Lesson is fun!";
+my $s = substr($str , 12 , 6 , "Perl");
+print "\$str = $str\n";
+print "\$s = $s\n";
+
+# index,rindex 全角は3index分、なんだ、、処理系にもよるのか？
+print index("Programming Lesson","ing") , "\n";
+print index("あいうえおあ","あ"), "\n";
+print rindex("あいうえおあ","あ"), "\n";
+print rindex("あaいうえおあa","a"), "\n";
+
+# 文字列置換 s///    どうやら先頭から最初にマッチしたものだけ置換されるらしい。詳しくは7章（=~、s///)
+my $str = "Programming Lesson is fun!Lesson";
+$str =~ s/Lesson/Perl/;
+print "\$str = $str\n";
